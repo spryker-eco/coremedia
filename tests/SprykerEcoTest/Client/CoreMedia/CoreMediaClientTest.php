c4ad0678c04db600071daa464a8ac93af1177efb
ilyakubanov <ilya.kubanov@spryker.com>
2019-09-04 17:49:02 +0300

    CMS-2670: added some mocks

--- tests/SprykerEcoTest/Client/CoreMedia/CoreMediaClientTest.php
+++ tests/SprykerEcoTest/Client/CoreMedia/CoreMediaClientTest.php
@@ -8,10 +8,17 @@
 namespace SprykerEcoTest\Client\CoreMedia;
 
 use Codeception\Test\Unit;
+use Generated\Shared\Transfer\CategoryNodeStorageTransfer;
 use SprykerEco\Client\CoreMedia\CoreMediaDependencyProvider;
+use SprykerEco\Client\CoreMedia\Dependency\Client\CoreMediaToCategoryStorageClientInterface;
+use SprykerEco\Client\CoreMedia\Dependency\Client\CoreMediaToProductStorageClientInterface;
 
 class CoreMediaClientTest extends Unit
 {
+    protected const PRODUCT_ABSTRACT_STORAGE_DATA = [];
+    protected const PRODUCT_CONCRETE_STORAGE_DATA = [];
+    protected const CATEGORY_STORAGE_DATA = [];
+
     /**
      * @var \SprykerEcoTest\Client\CoreMedia\CoreMediaClientTester
      */
@@ -33,5 +40,45 @@ class CoreMediaClientTest extends Unit
             CoreMediaDependencyProvider::CLIENT_PRODUCT_STORAGE,
             $this->getCoreMediaToProductStorageClientBridgeMock()
         );
+    }
+
+    /**
+     * @return void
+     */
+    protected function setCategoryStorageClientDependency(): void
+    {
+        $this->tester->setDependency(
+            CoreMediaDependencyProvider::CLIENT_CATEGORY_STORAGE,
+            $this->getCoreMediaToCategoryStorageClientBridgeMock()
+        );
+    }
+
+    /**
+     * @return \SprykerEco\Client\CoreMedia\Dependency\Client\CoreMediaToProductStorageClientInterface|\PHPUnit\Framework\MockObject\MockObject
+     */
+    protected function getCoreMediaToProductStorageClientBridgeMock(): CoreMediaToProductStorageClientInterface
+    {
+        $coreMediaToProductStorageClientBridge = $this->getMockBuilder(CoreMediaToProductStorageClientInterface::class)->getMock();
+        $coreMediaToProductStorageClientBridge->method('findProductAbstractStorageDataByMapping')->willReturn(
+            static::PRODUCT_ABSTRACT_STORAGE_DATA
+        );
+        $coreMediaToProductStorageClientBridge->method('findProductConcreteStorageDataByMapping')->willReturn(
+            static::PRODUCT_CONCRETE_STORAGE_DATA
+        );;
+
+        return $coreMediaToProductStorageClientBridge;
+    }
+
+    /**
+     * @return \SprykerEco\Client\CoreMedia\Dependency\Client\CoreMediaToCategoryStorageClientInterface|\PHPUnit\Framework\MockObject\MockObject
+     */
+    protected function getCoreMediaToCategoryStorageClientBridgeMock(): CoreMediaToCategoryStorageClientInterface
+    {
+        $coreMediaToCategoryStorageClientBridge = $this->getMockBuilder(CoreMediaToCategoryStorageClientInterface::class)->getMock();
+        $coreMediaToCategoryStorageClientBridge->method('getCategoryNodeById')->willReturn(
+            (new CategoryNodeStorageTransfer())->setUrl('category_url')
+        );
+
+        return $coreMediaToCategoryStorageClientBridge;
     }
 }
